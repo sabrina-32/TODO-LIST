b76e1fb8cd1da85684746a4c1f78ca068d9ecc93
Sabrina  Nusrat <43241001+sabrina-32@users.noreply.github.com>
2020-01-12 12:27:52 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 function newElement() {
-    var li = document.createElement("li");
+    var list = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    var text = document.createTextNode(inputValue);
+    list.appendChild(text);
     if (inputValue === '') {
       alert(" write something!");
     } else {
-      document.getElementById("myUL").appendChild(li);
+      document.getElementById("myUL").appendChild(list);
     }
     document.getElementById("myInput").value = "";
   
@@ -15,7 +15,7 @@ function newElement() {
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    li.appendChild(span);
+    list.appendChild(span);
   
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
